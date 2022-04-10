287b4d4c058adf74c0ab3618d53d49c1f64ce359
Sujith S <sujithx007@gmail.com>
2022-04-10 21:34:03 +0530

    Baseball solution

--- baseball_game/solution.py
+++ baseball_game/solution.py
@@ -1,8 +1,19 @@
+'''
+Solution to the baseball problem in leetcode
+'''
 class Solution:
+    '''
+    Main class : Solution to the baseball problem in leetcode
+    '''
     def calPoints(self, ops: List[str]) -> int:
+        '''
+        Main function.
+        Input : list of strings
+        Output : Score
+        '''
         final_list = []
         removed_val = 2
-        for n,val in enumerate(ops):
+        for val in ops:
             if val == 'C':
                 final_list.pop()
             elif val == 'D':
@@ -10,6 +21,6 @@ class Solution:
                 final_list.append(int(final_list_val))
             elif val == '+':
                 final_list.append(int(int(final_list[-1]) + int(final_list[-2])))
-            else : 
+            else :
                 final_list.append(int(val))
         return sum(final_list)
